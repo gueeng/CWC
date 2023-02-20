59a4b3c91c2c0af228f2bf51a1ac804079bbf420
Bibi <guenter.engelhard@me.com>
2023-02-20 16:39:41 +0100

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Guenter Engelhard on 20.02.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
